274ce46f6bc52a865de1624b06800b7c605a5356
Daniel Fernandez <mdanif9728@gmail.com>
2024-07-10 07:56:23 -0700

    Create a controller to handle the fetch data

--- models/band.js
+++ models/band.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 const {
   Model
 } = require('sequelize');
@@ -14,14 +14,37 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Band.init({
-    band_id: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    genre: DataTypes.TEXT,
-    available_start_time: DataTypes.DATE,
-    end_time: DataTypes.DATE
+    band_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    available_start_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    end_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    recommendation: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'Band',
-  });
-  return Band;
-};
+    tableName: 'Bands',
+    timestamps: false
+  })
+  return Band
+}
--- server.js
+++ server.js
@@ -9,16 +9,6 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// SEQUELIZE CONNECTION
-// const sequelize = new Sequelize(process.env.PG_URI)
-
-// try {
-//     sequelize.authenticate()
-//     console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
-// } catch (err) {
-//     console.log(`Unable to connect to PG: ${err}`)
-// }
-
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
@@ -26,6 +16,9 @@ app.get('/', (req, res) => {
     })
 })
 
+// CONTROLLERS
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
